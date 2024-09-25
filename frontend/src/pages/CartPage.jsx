f804e78747883016dc45dd5439b222ad16f2597d
dale_skinz <dale_skinner@hotmail.co.uk>
2024-09-25 14:04:22 +0100

    Update CartPage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -11,8 +11,8 @@ const CartPage = () => {
   const { cart } = useCartStore();
 
   return (
-    <div className='py-8 md:-py-16'>
-      <div className='mx-auto max-w-screen-lg px-4 2xl:px-0'>
+    <div className='py-8 md:py-16'>
+      <div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
         <div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
           <motion.div
             className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'
@@ -48,7 +48,6 @@ const CartPage = () => {
     </div>
   );
 };
-
 export default CartPage;
 
 const EmptyCartUI = () => (
